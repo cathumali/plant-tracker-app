4e469b50ddd20f5cb53f8da3bc89540d49980097
rinagreen <cathrinaumali@gmail.com>
2022-01-13 12:05:45 +0800

    updated css and config

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-    IMG_BASE_URL : "http://plant-tracker-app.test",
-    API_URL : "http://plant-tracker-app.test/api/plants"
+    IMG_BASE_URL : "http://plant-tracker-api.test",
+    API_URL : "http://plant-tracker-api.test/api/plants"
 }
   
   
