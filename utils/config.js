0ae577b59476e88814b192a23fd6730ab537f14c
rinagreen <cathrinaumali@gmail.com>
2022-01-13 17:24:57 +0800

    Added meta

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-    IMG_BASE_URL : "http://plant-tracker-api.rnc-studio.com.test",
-    API_URL : "http://plant-tracker-api.rnc-studio.com.test/api/plants"
+    IMG_BASE_URL : "http://plant-tracker-api.rnc-studio.com",
+    API_URL : "http://plant-tracker-api.rnc-studio.com/api/plants"
 }
   
   
